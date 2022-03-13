6bbb04f64ce6ff5005ed233aae23f2930a954ec2
Danielid Ramirez <danielid132@gmail.com>
2022-03-13 15:19:09 -0300

    add api

---
+++ app/controllers/api/v1/disney_controller.rb
@@ -0,0 +1,8 @@
+module Api
+    module V1
+        class DisneyController < V1Controller
+
+            
+        end
+    end
+end
---
+++ app/controllers/api/v1_controller.rb
@@ -0,0 +1,4 @@
+module Api
+    class V1Controller < ActionController::API
+    end
+end
